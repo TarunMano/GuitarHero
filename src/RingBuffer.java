8b22af533f152cf9e8b461a69849642d29ebedeb
TarunMano <111902945+TarunMano@users.noreply.github.com>
2022-11-29 11:47:05 -0600

    guitar string

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -1,4 +1,19 @@
 
 public class RingBuffer {
 
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String size() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void dequeue() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
