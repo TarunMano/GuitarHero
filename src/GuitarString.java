27a28609c26450031c82c96ad88d65311b95b348
TarunMano <111902945+TarunMano@users.noreply.github.com>
2022-11-29 11:59:14 -0600

    comit

--- src/GuitarString.java
+++ src/GuitarString.java
@@ -1,4 +1,23 @@
 
 public class GuitarString {
+
+	public GuitarString(double cONCERT_C) {
+		// TODO Auto-generated constructor stub
+	}
 //guitar 
+
+	public void pluck() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int sample() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void tic() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -1,17 +1,31 @@
 
 public class RingBuffer {
 
+	public RingBuffer(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public String size() {
+	public int size() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void dequeue() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void enqueue(double d) {
 		// TODO Auto-generated method stub
 		
 	}
