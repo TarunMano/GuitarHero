108fe6d67aa813f79cc39069a47801fe2fa22de0
TarunMano <111902945+TarunMano@users.noreply.github.com>
2022-12-02 12:12:00 -0600

    play method

--- src/guitarHero.java
+++ src/guitarHero.java
@@ -3,4 +3,13 @@ public class guitarHero {
 	public static void main(String[] args) {
 		
 	}
+	
+	private static void play(GuitarString[]list) {       
+        while (true) {
+        	if (StdDraw.hasNextKeyTyped()) {
+        		char key = StdDraw.nextKeyTyped();
+        		
+        	}
+        }
+	}
 }
