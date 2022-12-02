0ea62844e1fa4c7399a41a52ef3d193e8936235e
JGrade1 <111902996+JGrade1@users.noreply.github.com>
2022-12-02 12:11:45 -0600

    slkjf

--- src/guitarHero.java
+++ src/guitarHero.java
@@ -1,6 +1,9 @@
 
 public class guitarHero {
 	public static void main(String[] args) {
-		
+		 GuitarString[] list = new GuitarString[37];
+		 for (int i = 1; i <= list.length; i++) {
+			 list[i-1] = new GuitarString(440.0*(Math.pow(1.05956, i - 25)));
+		 }
 	}
 }
