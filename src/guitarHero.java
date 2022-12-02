f47713efda901058fc19cd934f2a69347f92a0f1
JGrade1 <111902996+JGrade1@users.noreply.github.com>
2022-12-02 12:36:38 -0600

    ssfsdf

--- src/guitarHero.java
+++ src/guitarHero.java
@@ -5,14 +5,33 @@ public class guitarHero {
 		 for (int i = 1; i <= list.length; i++) {
 			 list[i-1] = new GuitarString(440.0*(Math.pow(1.05956, i - 25)));
 		 }
+		 play(list);
 	}
 	
 	private static void play(GuitarString[]list) {       
         while (true) {
         	if (StdDraw.hasNextKeyTyped()) {
         		char key = StdDraw.nextKeyTyped();
-        		//k
+        		String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+        		int count = 0;
+        		for (char c : keyboard.toCharArray()) {
+        			if (key == c) {
+        				list[count].pluck();
+        			}
+        			count++;
+        		}
+        	}
+        	double sample = 0;
+        	for (int i = 0; i < list.length; i++) {
+        		sample += list[i].sample();
+        	}
+        	StdAudio.play(sample);
+        	
+        	for(int i = 0; i < list.length; i++) {
+        		list[i].tic();
         	}
         }
+  
+        
 	}
 }
